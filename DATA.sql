8d6b910a0e597e1eefc288206c52105f8c71d4b4
Ethan Patterson <159935067+EthanMPatterson@users.noreply.github.com>
2024-03-27 20:02:48 -0400

    adding changes

--- DATA.sql
+++ DATA.sql
@@ -1,59 +1,43 @@
 
-SELECT * FROM car
 --Creating a stored procedure to add cars to car
-CREATE OR REPLACE PROCEDURE add_car (vin VARCHAR, "year" INT, make VARCHAR, model VARCHAR, color VARCHAR, in_inventory BOOL, inventory_id INT, customer_id INT )
+CREATE OR REPLACE PROCEDURE add_car ("year" INT, make VARCHAR, model VARCHAR, color VARCHAR, in_inventory BOOL, customer_id INT)
 LANGUAGE plpgsql 
 AS $$
 BEGIN 
-	INSERT INTO car(vin, "year", make, model, color, in_inventory, inventory_id, customer_id)
-	VALUES (vin, "year", make, model, color, in_inventory, inventory_id, customer_id);
+	INSERT INTO car( "year", make, model, color, in_inventory, customer_id)
+	VALUES ("year", make, model, color, in_inventory, customer_id);
 END;
 $$;
 
-
---Adding data -- Using faker in python and found how to add a trigger so that a car will automatically be entered into the inventory unless it has a customer_id. 
--- That being said it created a mess so I ended up deleting it...I think I will just create a procedure to add all cars with customer_id NULL to inventory after the fact instead of trying to get the trigger to automatically do it. 
-
-
 --Base reqs
+--CALL add_car(:year, :make, :model, :color, :in_inventory, :inventory_id, :customer_id) 
 
-CALL add_car(:vin, :year, :make, :model, :color, :in_inventory, :inventory_id, :customer_id) 
--- Adding Cars. Used faker in python to print out an f'string to easily past in call function. 
 
---										EXAMPLE:
+CALL add_car(2006, 'Suzuki', 'Verona', 'Beige', TRUE, NULL);
 
---	(venv) PS C:\Users\ethan\Documents\codingtemple\week5\W5PRJCT> py data_generations 
--- 	33MFRS6TXBVKRBJVJ ['2006', 'Hyundai', 'Elantra'] Gold
---  CALL add_car('33MFRS6TXBVKRBJVJ', 2006, 'Hyundai', 'Elantra', 'Gold', TRUE, NULL, NULL)
+CALL add_car(2015, 'Ford', 'C-MAX', 'Olive', TRUE, NULL);
 
-CALL add_car('0ASNGG5M2CZU8Z3TX', 2006, 'Suzuki', 'Verona', 'Beige', TRUE, NULL, NULL)
+CALL add_car(2006, 'GMC', 'Canyon', 'DarkSlateBlue', TRUE, NULL);
 
-CALL add_car('4SFH6TZWXK1RJYZW3', 2015, 'Ford', 'C-MAX', 'Olive', TRUE, NULL, NULL)
+CALL add_car(1999, 'Mercury', 'Sable', 'MediumSlateBlue', TRUE, NULL);
 
-CALL add_car('E7YMKRCE4T93JRWM0', 2006, 'GMC', 'Canyon', 'DarkSlateBlue', TRUE, NULL, NULL)
+CALL add_car(2010, 'Ford', 'F150', 'Pink', TRUE, NULL);
 
-CALL add_car('BEVU2PL05VF0P95EL', 1999, 'Mercury', 'Sable', 'MediumSlateBlue', TRUE, NULL, NULL)
+CALL add_car(2019, 'Nissan', 'NV3500', 'LightSkyBlue', TRUE, NULL);
 
-CALL add_car('X01RJ6RF97AAC2AGW', 2010, 'Ford', 'F150', 'Pink', TRUE, NULL, NULL)
+CALL add_car(1998, 'Dodge', 'Neon', 'SandyBrown', TRUE, NULL);
 
-CALL add_car('LD71NV7S3H2BAFGSK', 2019, 'Nissan', 'NV3500', 'LightSkyBlue', TRUE, NULL, NULL)
+CALL add_car(2010, 'Honda', 'Ridgeline', 'DarkOrange', TRUE, NULL);
 
-CALL add_car('9MP0WECU9ZTNAR23P', 1998, 'Dodge', 'Neon', 'SandyBrown', TRUE, NULL, NULL)
+CALL add_car(1999, 'Toyota', 'Camry', 'GhostWhite', TRUE, NULL);
 
-CALL add_car('7AKKWG6FXUXVH4TEB', 2010, 'Honda', 'Ridgeline', 'DarkOrange', TRUE, NULL, NULL)
+CALL add_car(2013, 'Lexus', 'ES', 'AntiqueWhite', TRUE, NULL);
 
-CALL add_car('GJ6RYBVJ4KCGHCR6F', 1999, 'Toyota', 'Camry', 'GhostWhite', TRUE, NULL, NULL) -
-CALL add_car('6CGAY494XPVMTEKAX', 2013, 'Lexus', 'ES', 'AntiqueWhite', TRUE, NULL, NULL)
+CALL add_car( 2001, 'Buick', 'Regal', 'LightBlue', TRUE, NULL);
 
-CALL add_car('SPJ235AF0Y713CLGP', 2001, 'Buick', 'Regal', 'LightBlue', TRUE, NULL, NULL)
+CALL add_car(2008, 'Chevrolet', 'Tahoe', 'DarkGoldenRod', TRUE, NULL);
 
-CALL add_car('6F2BAKD6610KDVFDH', 2008, 'Chevrolet', 'Tahoe', 'DarkGoldenRod', TRUE, NULL, NULL)
-
-CALL add_car('3JY29ZJ395GZTZ29G', 2014, 'Freightliner', 'Sprinter', 'LightSlateGray', TRUE, NULL, NULL)
-
-CALL add_car('RT3SEVL085W8WM76J', 2005, 'Toyota', '4Runner', 'MediumTurquoise', TRUE, NULL, NULL)
-
-SELECT * FROM car 
+CALL add_car(2014, 'Freightliner', 'Sprinter', 'LightSlateGray', TRUE, NULL);
 
 
 -- Adding a few customers 
@@ -69,106 +53,76 @@ VALUES ('Valerie', 'Lee', 'christinawilliams@example.net', '405-800-5993'),
 ('Sarah', 'Huber', 'jasongrant@example.org', '708-251-7748');
 
 -- Adding some customer vehicles 
-CALL add_car('RT3SEVL085W8WM76J', 2005, 'Toyota', '4Runner', 'MediumTurquoise', FALSE, NULL, 3)
+CALL add_car(2005, 'Toyota', '4Runner', 'MediumTurquoise', FALSE, 3);
 
-CALL add_car('4M371SNZ9YDCZPV6R', 2009, 'Buick', 'LaCrosse', 'PaleGreen', FALSE, NULL, 6)
+CALL add_car(2009, 'Buick', 'LaCrosse', 'PaleGreen', FALSE, 6);
 
-CALL add_car('3AD6BV9A1ALYA7N54', 2008, 'Saturn', 'SKY', 'DarkOliveGreen', FALSE, NULL, 7)
-
-
-SELECT * FROM customer
+CALL add_car(2008, 'Saturn', 'SKY', 'DarkOliveGreen', FALSE, 7);
 
 
-SELECT * FROM car
+--Procedure to add sales employee
 
--- Adding 2 departments 
-SELECT * FROM department d 
-
-INSERT INTO department (department_name)
-VALUES ('Sales'), ('Service');
-
--- Creating procedure to add employee 
-
-CREATE OR REPLACE PROCEDURE add_employee (first_name VARCHAR, last_name VARCHAR, department_id INT)
+CREATE OR REPLACE PROCEDURE add_sales (first_name VARCHAR, last_name VARCHAR)
 LANGUAGE plpgsql
 AS $$
 BEGIN 
-	INSERT INTO employee(first_name, last_name, department_id)
-	VALUES (first_name, last_name, department_id);
+	INSERT INTO sales(first_name, last_name)
+	VALUES (first_name, last_name);
+END;
+$$;
+
+--Procedure to add mechanic employee 
+
+CREATE OR REPLACE PROCEDURE add_mechanic(first_name VARCHAR, last_name VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN 
+	INSERT INTO mechanic(first_name, last_name)
+	VALUES (first_name, last_name);
 END;
 $$;
 
 -- Adding Employees
 
-CALL add_employee('Jared', 'Gilbert', 2);
-CALL add_employee('Christopher', 'Garcia', 2);
-CALL add_employee('Kathryn', 'Jones', 1);
-CALL add_employee('Lori', 'Beasley', 1);
-CALL add_employee('Ronald', 'Doyle', 1); 
-CALL add_employee('Austin', 'Rios', 2);
-CALL add_employee('Steven', 'Reid', 2);
-CALL add_employee('William', 'Jarvis', 1);
-
- --Verifying
-SELECT * FROM employee e
-
--- Created stored procedure to add all cars that have a customer_id of NULL to inventory 
-CREATE OR REPLACE PROCEDURE add_cars_to_inventory()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    INSERT INTO inventory (car_vin)
-    SELECT vin
-    FROM Car
-    WHERE customer_id IS NULL;
-END;
-$$;
-
-CALL add_cars_to_inventory();
---Verifying
-SELECT * FROM inventory;
-SELECT * FROM car;
-SELECT * FROM customer;
-SELECT * FROM employee e ;
-
---Need to add data to service_mechanic, service_ticket, invoice. 
-
-SELECT * FROM service_ticket*
-('RT3SEVL085W8WM76J', 2005, 'Toyota', '4Runner', 'MediumTurquoise', FALSE, NULL, 3)
-
-('4M371SNZ9YDCZPV6R', 2009, 'Buick', 'LaCrosse', 'PaleGreen', FALSE, NULL, 6)
-
-('3AD6BV9A1ALYA7N54', 2008, 'Saturn', 'SKY', 'DarkOliveGreen', FALSE, NULL, 7)
-
-SELECT * FROM customer
-JOIN car ON customer.id = car.customer_id ;
+CALL add_mechanic('Jared', 'Gilber');
+CALL add_mechanic('Christopher', 'Garcia');
+CALL add_sales('Kathryn', 'Jones');
+CALL add_sales('Lori', 'Beasley');
+CALL add_mechanic('Ronald', 'Doyle'); 
+CALL add_mechanic('Austin', 'Rios');
+CALL add_sales('Steven', 'Reid');
+CALL add_sales('William', 'Jarvis');
 
 
 INSERT INTO service(service_type, rate)
 VALUES ('oil change', 39.99);
 
-INSERT INTO service_ticket (car_vin, customer_id, service_id, notes, date_completed)
-VALUES ('RT3SEVL085W8WM76J', 3, 1, 'Oil drained and replaces. Breaks checked. Good to go.', '03/24/2024');
-
-INSERT INTO service_mechanic(service_id, ticket_id, employee_id, hours, note)
-VALUES (1, 3, 2, 1,'completed oil change');
+INSERT INTO service_ticket (car_id, customer_id, service_id, mechanic_id, notes, date_completed)
+VALUES (2, 3, 1, 2, 'Oil drained and replacd. Breaks checked. Good to go.', '03/24/2024');
 
 INSERT INTO service(service_type, rate)
 VALUES ('tire rotation and alignment', 99.99);
 
-INSERT INTO service_ticket (car_vin, customer_id, service_id, notes, date_completed)
-VALUES ('4M371SNZ9YDCZPV6R', 6, 2, 'passenger side showing excessive wear', '03/24/2024');
+INSERT INTO service_ticket (car_id, customer_id, service_id, mechanic_id, notes, date_completed)
+VALUES (3, 6, 2, 4, 'passenger side showing excessive wear', '03/24/2024');
+
+INSERT INTO invoice ("date", customer_id, sales_id, car_id)
+VALUES ('3/26/24', 5, 2, 5);
+
+INSERT INTO invoice ("date", customer_id, sales_id, car_id)
+VALUES ('3/25/24', 4, 1, 13);
 
 
 
+-- Verifying data in each table
 
-INSERT INTO service_mechanic(service_id, ticket_id, employee_id, hours, note)
-VALUES (2, 4, 6, 2,'passenger side showing excessive wear');
-
-
-
-SELECT * FROM service_mechanic sm 
-
+SELECT * FROM car;
+SELECT * FROM customer;
+SELECT * FROM invoice;
+SELECT * FROM mechanic; -- Works but red, underlined, and italicized???? Maybe just a dbeaver error? 
+SELECT * FROM sales;    -- 						//
+SELECT * FROM service;
+SELECT * FROM service_ticket;
 
 
 
--- W5PRJCT.Dealership.sql
+++ W5PRJCT.Dealership.sql
@@ -1,16 +1,16 @@
 
-CREATE TABLE department (
-id SERIAL PRIMARY KEY,
-department_name VARCHAR(30)
-);
-
-CREATE TABLE employee (
+CREATE TABLE sales (
 id SERIAL PRIMARY KEY,
 first_name VARCHAR(30),
-last_name VARCHAR(30),
-department_id INT,
-FOREIGN KEY (department_id) REFERENCES department(id)
+last_name VARCHAR(30)
 );
+
+CREATE TABLE mechanic (
+id SERIAL PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR (30)
+);
+
 
 CREATE TABLE service (
 id SERIAL PRIMARY KEY, 
@@ -19,15 +19,13 @@ rate NUMERIC(5,2)
 );
 
 CREATE TABLE car( 
-vin VARCHAR(17) PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 "year" INT,
 make VARCHAR(25), 
 model VARCHAR(25),
 color VARCHAR(25),
 in_inventory BOOL,
-inventory_id INT,
 customer_id INT
---FOREIGN KEY (inventory_id) REFERENCES inventory(id)
 --FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
@@ -37,54 +35,39 @@ first_name VARCHAR(20),
 last_name VARCHAR(30),
 email VARCHAR(100),
 phone VARCHAR(13),
-car_vin VARCHAR(17)
---FOREIGN KEY (car_vin) REFERENCES car(vin)
+car_id INT
+--FOREIGN KEY (car_id) REFERENCES car(id)
 );
 
-CREATE TABLE inventory ( 
-id SERIAL PRIMARY KEY, 
-is_used BOOL,
-car_vin VARCHAR(17),
-FOREIGN KEY (car_vin) REFERENCES car(vin)
-);
 
 CREATE TABLE invoice ( 
 id SERIAL PRIMARY KEY,
-"total" NUMERIC(9,2),
 "date" VARCHAR(10),
 customer_id INT,
-employee_id INT,
+sales_id INT,
+car_id INT,
 FOREIGN KEY (customer_id) REFERENCES customer(id),
-FOREIGN KEY (employee_id) REFERENCES employee(id)
+FOREIGN KEY (sales_id) REFERENCES sales(id),
+FOREIGN KEY (car_id) REFERENCES car(id)
 );
 
 CREATE TABLE service_ticket ( 
 id SERIAL PRIMARY KEY, 
-car_vin VARCHAR(17),
+car_id INT,
 customer_id INT,
 service_id INT,
+mechanic_id INT,
 notes TEXT,
 date_completed VARCHAR(10),
-FOREIGN KEY (car_vin) REFERENCES car(vin),
+FOREIGN KEY (mechanic_id) REFERENCES mechanic(id),
+FOREIGN KEY (car_id) REFERENCES car(id),
 FOREIGN KEY (customer_id) REFERENCES customer(id),
 FOREIGN KEY (service_id) REFERENCES service(id)
 );
 
-CREATE TABLE service_mechanic (
-id SERIAL PRIMARY KEY,
-service_id INT,
-ticket_id INT,
-employee_id INT,
-hours INT,
-note TEXT,
-FOREIGN KEY (service_id) REFERENCES service(id),
-FOREIGN KEY (ticket_id) REFERENCES service_ticket(id),
-FOREIGN KEY (employee_id) REFERENCES employee(id)
-);
 
 ALTER TABLE customer 
-ADD FOREIGN KEY (car_vin) REFERENCES car(vin);
+ADD FOREIGN KEY (car_id) REFERENCES car(id);
 
 ALTER TABLE car 
-ADD FOREIGN KEY (inventory_id) REFERENCES inventory(id),
 ADD FOREIGN KEY (customer_id) REFERENCES customer(id); 
